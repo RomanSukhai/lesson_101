df80ce6df196d17678dc84e38295af1db75c2da1
Roman <romansukhai@gmail.com>
2021-08-20 16:01:07 +0300

    add3

--- src/homework_2/Main2.java
+++ src/homework_2/Main2.java
@@ -1,8 +1,6 @@
 package homework_2;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main2 {
@@ -10,9 +8,15 @@ public class Main2 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введіть речення/'Я замінб голосні на пробіл/'");
         String text = sc.next();
-
-    if ((text.indexOf('e') > -1)||(text.indexOf('a') > -1)||(text.indexOf('u') > -1)||(text.indexOf('o') > -1)){
-        System.out.println(text.replace('e','-').replace('a','-').replace('o','-').replace('u','-'));
+        boolean a = text.equalsIgnoreCase(text.replace('e','-'));
+    if ((text.indexOf('e') > -1)||(text.indexOf('a') > -1)||
+            (text.indexOf('u') > -1)||(text.indexOf('o') > -1)||
+            (text.indexOf('E') > -1)||(text.indexOf('A') > -1)||
+            (text.indexOf('U') > -1)||(text.indexOf('O') > -1)){
+        System.out.println(text.replace('e','-')
+                .replace('a','-').replace('o','-').replace('u','-')
+                .replace('E','-').replace('A','-').replace('O','-')
+                .replace('U','-'));
     }
     else {
         System.out.println(text);
